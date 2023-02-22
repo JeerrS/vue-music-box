47ea014481cd781395efd04b9049216d76602225
Jerry Sun <july_sunjun@outlook.com>
2023-02-22 21:22:35 +0800

    [New] Init the project
    - 新增
     - 初始化项目

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+    <div id="app">
+        <ice-header>
+            <template v-slot:left>
+                <img src="./assets/header_dark.svg" alt="icon">
+            </template>
+            <template v-slot:middle>
+                <div class="middle-button-group">
+                    <div class="navigator-group">
+                    </div>
+                    <div class="search-group"></div>
+                </div>
+            </template>
+            <template v-slot:right>
+                <div class="profile"></div>
+            </template>
+        </ice-header>
+    </div>
+</template>
+
+<style lang="scss">
+body {
+    margin: 0;
+    background: #05020e;
+}
+
+#app {
+    height: 100vh;
+}
+.profile {
+    height: 26px;
+    width: 26px;
+    border-radius: 50%;
+    background: white;
+}
+.middle-button-group {
+    height: 48px;
+    width: 248px;
+    background: #fff;
+}
+</style>
+<script>
+import IceHeader from '@/components/IceHeader/index.vue';
+
+export default {
+    components: {IceHeader}
+};
+
+</script>
---
+++ src/components/IceHeader/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="ice-header">
+        <div class="ice-left">
+            <slot name="left"></slot>
+        </div>
+        <div class="ice-middle">
+            <slot name="middle"></slot>
+        </div>
+        <div class="ice-right">
+            <slot name="right"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'IceHeader'
+};
+</script>
+
+<style lang="scss" scoped>
+.ice-header {
+    width: 100%;
+    box-sizing: border-box;
+    padding: 0 16px;
+    height: 64px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+</style>
